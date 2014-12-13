6488fa3d6b9d7d0fe6c294b78f6cb597d167bc70
tuliang <info@tuliang.org>
2014-12-13 20:20:03 +0800

    suppotr environment

--- simple.rb
+++ simple.rb
@@ -3,13 +3,12 @@ require_relative "simple/add"
 require_relative "simple/multiply"
 require_relative "simple/boolean"
 require_relative "simple/less_than"
+require_relative "simple/variable"
 require_relative "simple/machine"
 
 
 Machine.new(
-  LessThan.new(
-    Number.new(5), 
-    Add.new(Number.new(2), Number.new(1))
-  )
+  Add.new(Variable.new(:x), Variable.new(:y)),
+  { x: Number.new(3), y: Number.new(4) }
 ).run
 
--- simple/machine.rb
+++ simple/machine.rb
@@ -1,6 +1,8 @@
-class Machine < Struct.new(:expression)
+# Object.send(:remove_const, :Machine) # 忘记原来的 Machine 类
+
+class Machine < Struct.new(:expression, :environment)
   def step
-    self.expression = expression.reduce
+    self.expression = expression.reduce(environment)
   end
 
   def run
