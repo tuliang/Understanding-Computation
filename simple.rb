1c7ed18f0068d95ff7d2123cc514510302f21a75
tuliang <info@tuliang.org>
2014-12-13 21:26:48 +0800

    fix if

--- simple.rb
+++ simple.rb
@@ -6,11 +6,15 @@ require "./simple/less_than"
 require "./simple/variable"
 require "./simple/do_nothing"
 require "./simple/assign"
+require "./simple/if"
 require "./simple/machine"
 
 
 Machine.new(
-  Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
-  { x: Number.new(2) }
+  If.new(
+    Variable.new(:x),
+    Assign.new(:y, Number.new(1)),
+    Assign.new(:y, Number.new(2))
+  ),
+  { x: Boolean.new(true) }
 ).run
-
